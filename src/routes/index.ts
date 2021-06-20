e2b7025fe1b81ee8e6fe4491a2e2e609c9de6593
Bruno Henrique de Borba <brunohenriqueborba@gmail.com>
2021-06-20 15:06:24 -0300

    refactoring routes

---
+++ src/routes/index.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+
+import { categoriesRoutes } from './categories.routes';
+import { specificationsRoutes } from './specifications.routes';
+
+const router = Router();
+
+// REST
+
+router.use('/categories', categoriesRoutes);
+
+router.use('/specifications', specificationsRoutes);
+
+export { router };
--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,14 @@ import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
 
 import schema from './graphql/schemasMap';
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specifications.routes';
+import { router } from './routes';
 
 const app = express();
 
-// REST
-
 app.use(express.json());
 
-app.use('/categories', categoriesRoutes);
-
-app.use('/specifications', specificationsRoutes);
+// REST
+app.use(router);
 
 // GraphQL
 
