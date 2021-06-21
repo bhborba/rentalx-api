be19980934e5e253685ecf04be7c499887768ec3
bhborba <brunohenriqueborba@gmail.com>
2021-06-21 10:09:08 -0300

    Reading files from upload

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,27 +1,54 @@
 import csvParse from 'csv-parse';
 import fs from 'fs';
 
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
+interface IImportCategory {
+    name: string;
+    description: string;
+}
+
 class ImportCategoryUseCase {
-    async execute(file: any): Promise<void> {
+    constructor(private categoriesRepository: ICategoriesRepository) {}
+
+    async loadCategories(file: any): Promise<IImportCategory[]> {
         const { createReadStream, filename, path } = await file;
-        const parseFile = csvParse();
-        let stream = null;
+        return new Promise((resolve, reject) => {
+            const categories: IImportCategory[] = [];
+            const parseFile = csvParse();
+            let stream = null;
 
-        // Se veio de REST
-        if (path) {
-            console.log('entrou onde nao devia');
-            stream = fs.createReadStream(file.path);
-        } else {
-            stream = createReadStream();
+            // Se veio de REST
+            if (path) {
+                console.log('entrou onde nao devia');
+                stream = fs.createReadStream(file.path);
+            } else {
+                stream = createReadStream();
 
-            stream.pipe(fs.createWriteStream(`./tmp/${filename}`));
-        }
+                stream.pipe(fs.createWriteStream(`./tmp/${filename}`));
+            }
 
-        stream.pipe(parseFile);
+            stream.pipe(parseFile);
 
-        parseFile.on('data', async line => {
-            console.log(line);
+            parseFile
+                .on('data', async line => {
+                    const [name, description] = line;
+                    categories.push({
+                        name,
+                        description,
+                    });
+                })
+                .on('end', () => {
+                    resolve(categories);
+                })
+                .on('error', err => {
+                    reject(err);
+                });
         });
+    }
+    async execute(file: any): Promise<void> {
+        const categories = await this.loadCategories(file);
+        console.log(categories);
     }
 }
 
