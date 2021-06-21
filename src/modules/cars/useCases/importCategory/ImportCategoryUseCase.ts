2aa8eabd30d161d02368c0a5c94a2a4a029a93be
bhborba <brunohenriqueborba@gmail.com>
2021-06-21 09:47:58 -0300

    stream concept

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,27 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+
 class ImportCategoryUseCase {
-    execute(file: any) {
-        console.log(file);
+    async execute(file: any): Promise<void> {
+        const { createReadStream, filename, path } = await file;
+        const parseFile = csvParse();
+        let stream = null;
+
+        // Se veio de REST
+        if (path) {
+            console.log('entrou onde nao devia');
+            stream = fs.createReadStream(file.path);
+        } else {
+            stream = createReadStream();
+
+            stream.pipe(fs.createWriteStream(`./tmp/${filename}`));
+        }
+
+        stream.pipe(parseFile);
+
+        parseFile.on('data', async line => {
+            console.log(line);
+        });
     }
 }
 
